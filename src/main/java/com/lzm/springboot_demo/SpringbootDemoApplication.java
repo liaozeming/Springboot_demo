0f5496f45bccbcfc654002f56f5b92cd0b74d9b1
lzm <1329835847@qq.com>
2021-05-07 15:23:06 +0800

    second

--- src/main/java/com/lzm/springboot_demo/SpringbootDemoApplication.java
+++ src/main/java/com/lzm/springboot_demo/SpringbootDemoApplication.java
@@ -6,6 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
+    /*
+    * @Description: TODO
+    * @author: lzm
+    * @date: 2021/5/7 15:16
+    * @param args: 
+    * @Return: void
+    */
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
